80d92d1e5d39ae64cfb60fe2d77c6997400db9fa
camiloceron <ikamc014@gmail.com>
2015-04-08 08:38:22 -0500

    primera subida

---
+++ Documents/NetBeansProjects/JSF/src/java/com/JSF/RegistrationBean.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.JSF;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Ingenieria
+ */
+@ManagedBean
+@RequestScoped
+public class RegistrationBean {
+
+    private String saludo;
+    private String nombre;
+    private String apellidos;
+    private Integer edad;
+    private String email;
+
+    public String getSaludo() {
+        return saludo;
+    }
+
+    public void setSaludo(String saludo) {
+        this.saludo = saludo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }       
+            
+    public RegistrationBean() {
+    }
+    
+}
